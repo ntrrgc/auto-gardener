bd7575d51e9b2a3a48ec43705b0bd9b44d1ec962
Alicia Boya García <ntrrgc@gmail.com>
2017-10-12 14:28:36 +0200

    Refactor parsers to separate files

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
-import * as fs from "fs";
-import {ensure, groupBy, maxBy, sortedBy} from "./functional-utils";
+import {groupBy, sortedBy} from "./functional-utils";
 import {printVtLines, VtLine, vtPadLeft} from "./vt-utils";
+import {parseExpectations} from "./parse-expectations";
+import {constructBotTestsResultsFromJson} from "./parse-results-json";
 
-const dontShowUnexpectedPasses = false;
-
-enum TestOutcome {
+export enum TestOutcome {
     NoData,
     Pass,
     Failure,
@@ -18,7 +17,7 @@ enum TestOutcome {
     DumpJSConsoleLogInStdErr,
 }
 
-function testOutcomeToLetter(outcome: TestOutcome): string {
+export function testOutcomeToLetter(outcome: TestOutcome): string {
     switch (outcome) {
         case TestOutcome.NoData:
             return "N";
@@ -41,7 +40,7 @@ function testOutcomeToLetter(outcome: TestOutcome): string {
     }
 }
 
-function testOutcomeToColor(outcome: TestOutcome, colorType: "bg" | "fg"): string {
+export function testOutcomeToColor(outcome: TestOutcome, colorType: "bg" | "fg"): string {
     const prefix = colorType == "bg" ? "\x1b[48;5;" : "\x1b[38;5;";
     switch (outcome) {
         case TestOutcome.NoData:
@@ -64,18 +63,18 @@ function testOutcomeToColor(outcome: TestOutcome, colorType: "bg" | "fg"): strin
     }
 }
 
-enum BuildType {
+export enum BuildType {
     Debug,
     Release
 }
 
-enum ToStringMode {
+export enum ToStringMode {
     Normal = 0,
     WithColors = 1,
     PadBugLink = 2,
 }
 
-class TestExpectation {
+export class TestExpectation {
     constructor(
         public lineNo: number,
         public testPath: Path,
@@ -131,7 +130,7 @@ class TestExpectation {
     }
 }
 
-class Path {
+export class Path {
     constructor(public entries: string[]) {
     }
 
@@ -165,189 +164,26 @@ class Path {
     }
 }
 
-function consumeWordToken(line: string): [string, string | null] {
-    line = line.trim();
-    if (!line) {
-        return [line, null];
-    }
 
-    const match = /(.*?)\s*(\S+)$/.exec(line);
-    if (!match) {
-        throw new Error(`Could not find word: "${line}"`);
-    }
-
-    return [match[1], match[2]];
-}
-
-function consumeBracketedEntityToken(line: string): [string, string | null] {
-    line = line.trim();
-    if (!line) {
-        return [line, null];
-    }
-
-    const match = /(.*?)\s*\[\s*([^\]]+?)\s*]$/.exec(line);
-    if (!match) {
-        return [line, null]
-    }
-
-    return [match[1], match[2]];
-}
-
-function consumeBracketedEntityTokenUnitTests() {
-    function check(a: [string, string|null], b: [string, string|null]) {
-        if (a[0] != b[0]) {
-            console.warn(`Expected line "${a[0]}" == "${b[0]}"`);
-        }
-        if (a[1] != b[1]) {
-            console.warn(`Expected token "${a[1]}" == "${b[1]}"`);
-        }
-    }
-    check(consumeWordToken("aa bb"), ["aa", "bb"]);
-    check(consumeWordToken("aa b/xxx/c "), ["aa", "b/xxx/c"]);
-    check(consumeWordToken("zz"), ["", "zz"]);
-    check(consumeWordToken(""), ["", null]);
-    check(consumeBracketedEntityToken("bb [ Pass Fail ]"), ["bb", "Pass Fail"]);
-    check(consumeBracketedEntityToken("bb [ Pass] "), ["bb", "Pass"]);
-    check(consumeBracketedEntityToken("cc dd"), ["cc dd", null]);
-    check(consumeBracketedEntityToken("cc dd "), ["cc dd", null]);
-}
-
-function parseExpectations(filePath: string): TestExpectation[] {
-    const fileText: string = fs.readFileSync(filePath, "UTF-8");
-    if (fileText == null) {
-        throw new Error("Could not read expectations file");
-    }
-    const lines = fileText.split("\n");
-
-    const collectedExpectations = new Array<TestExpectation>()
-    let lineNo = 0;
-
-    for (let line of lines) {
-        lineNo++;
-
-        // Remove comments
-        line = line.split("#", 1)[0];
-        // Clean whitespace
-        line = line.trim();
-
-        if (line == "")
-            continue;
-
-        let outcomes: Set<TestOutcome>;
-        let outcomesString: string | null;
-        [line, outcomesString] = consumeBracketedEntityToken(line);
-        if (outcomesString) {
-            outcomes = new Set<TestOutcome>(outcomesString
-                .split(" ")
-                .map(x => x.trim())
-                .filter(x => x != "")
-                .map(str => ensure(TestOutcome[str as keyof typeof TestOutcome],
-                    `Unknown outcome at line ${lineNo}: "${str}"`))
-                .filter(x => x != TestOutcome.DumpJSConsoleLogInStdErr));
-
-            if (outcomes.size == 0) {
-                outcomes = new Set([TestOutcome.Pass]);
-            }
-            if (dontShowUnexpectedPasses) {
-                outcomes.add(TestOutcome.Pass);
-            }
-        } else {
-            // console.warn(`Could not parse expected outcomes at line ${lineNo}: "${line}"`);
-            // Consider as Skip
-            outcomes = new Set([TestOutcome.Skip]);
-        }
-
-        let testPathString: string | null;
-        [line, testPathString] = consumeWordToken(line);
-        if (!testPathString) {
-            console.warn(`Could not parse test path at line ${lineNo}: "${line}"`);
-            continue;
-        }
-        const testPath = new Path(testPathString.split("/"));
-
-        let buildTypeConstraintString: string | null;
-        [line, buildTypeConstraintString] = consumeBracketedEntityToken(line);
-        let buildTypeConstraint: BuildType | null;
-        if (buildTypeConstraintString) {
-            buildTypeConstraint = ensure(BuildType[buildTypeConstraintString as keyof typeof BuildType],
-                `Unknown build type at line ${lineNo}: "${buildTypeConstraintString}"`);
-        } else {
-            // No build type constraint
-            buildTypeConstraint = null;
-        }
-
-        let bugIds = new Array<number>();
-        while (line != "") {
-            let bugString: string | null;
-            [line, bugString] = consumeWordToken(line);
-            if (bugString) {
-                const match = /^webkit.org\/b\/(\d+)\s*$/.exec(bugString);
-                if (match) {
-                    const bugId = parseInt(match[1]);
-                    if (Number.isNaN(bugId)) {
-                        throw new Error(`Invalid bug number: "${match[1]}"`);
-                    }
-                    bugIds.push(bugId);
-                }
-            }
-        }
-
-        if (line != "") {
-            throw new Error(`Unparsed line contents remain at line ${lineNo}: "${line}"`);
-        }
-
-        const newExpectation = new TestExpectation(lineNo, testPath, bugIds, outcomes, buildTypeConstraint)
-        collectedExpectations.push(newExpectation);
-    }
-
-    return collectedExpectations;
-}
-
-
-interface JSONTestsResultsRoot {
-    [platformName: string]: JSONTestsResultsPlatform;
-}
-interface JSONTestsResultsPlatform {
-    tests: JSONTestDirectory;
-    buildNumbers: string[];
-    webkitRevision: string[];
-}
-interface JSONTestDirectory {
-    [nodeName: string]: JSONTestDirectory | JSONTest;
-}
-interface JSONTest {
-    results: JSONTestOutcomeHistoryEntry[],
-    times: JSONTestTimesHistoryEntry[];
-}
-type JSONTestOutcomeLetter = string;
-type JSONTestOutcomeHistoryEntry = [
-    number, // number of occurrences
-    JSONTestOutcomeLetter // outcome
-];
-interface JSONTestTimesHistoryEntry {
-    0: number; // number of occurrences
-    1: number; // time in seconds
-}
-
-interface TestContext {
+export interface TestContext {
     platform: "gtk";
     buildType: BuildType;
 }
 
-interface TestResult {
+export interface TestResult {
     webkitRevision: number;
     outcome: TestOutcome;
 }
 
-interface BotsTestResults {
+export interface BotsTestResults {
     context: TestContext;
     webkitRevisions: number[]; //most recent first
     testHistories: TestHistory[];
 }
 
-type RevisionRange = number | {start: number, end: number} | "long ago" | "never failed";
+export type RevisionRange = number | {start: number, end: number} | "long ago" | "never failed";
 
-class TestHistory {
+export class TestHistory {
     constructor(public context: TestContext,
                 public testPath: Path,
                 public lastResults: TestResult[], // most recent first
@@ -482,92 +318,6 @@ class TestHistory {
     }
 }
 
-function getTestsResultsJsonPlatformName(jsonRoot: JSONTestsResultsRoot) {
-    for (let key in jsonRoot) {
-        if (key != "version") {
-            return key;
-        }
-    }
-    throw new Error("Could not find platform name in tests results JSON root.");
-}
-
-function parseOutcomeString(outcomeString: JSONTestOutcomeLetter): TestOutcome {
-    const outcomeDict: {[key: string]: TestOutcome} = {
-        "N": TestOutcome.NoData, // no data,
-        "P": TestOutcome.Pass,
-        "F": TestOutcome.Failure,
-        "C": TestOutcome.Crash,
-        "T": TestOutcome.Timeout,
-        "I": TestOutcome.ImageOnlyFailure,
-        "A": TestOutcome.Failure, // AudioOnlyFailure?
-        "O": TestOutcome.Missing,
-    };
-    if (!(outcomeString in outcomeDict)) {
-        throw new Error(`Unknown test outcome string: "${outcomeString}"`);
-    }
-    return outcomeDict[outcomeString];
-}
-
-function findMostSpecificExpectation(allExpectations: TestExpectation[],
-                                     testPath: Path,
-                                     buildType: BuildType): TestExpectation | null
-{
-    const matches = allExpectations.filter(expectation => expectation.matchesTest(testPath, buildType));
-    return maxBy(matches, expectation => expectation.testPath.entries.length);
-}
-
-function constructBotTestsResultsFromJson(context: TestContext, releaseJson: JSONTestsResultsRoot): BotsTestResults {
-    const jsonReleasePlatform = releaseJson[getTestsResultsJsonPlatformName(releaseJson)];
-    const collectedTestHistories = new Array<TestHistory>();
-    const webkitRevisions = jsonReleasePlatform.webkitRevision.map(x => parseInt(x));
-    if (webkitRevisions[0] <= webkitRevisions[1]) throw new Error("assertion error");
-
-    function collectTestHistory(testPathNodes: string[], jsonTest: JSONTest) {
-        const testPath = new Path(testPathNodes);
-        const expectation = findMostSpecificExpectation(allExpectations, testPath, context.buildType);
-
-        const lastResults = new Array<TestResult>();
-        let webkitRevisionIndex = 0;
-        for (let [occurrences, outcomeString] of jsonTest.results) {
-            const outcome = parseOutcomeString(outcomeString);
-            for (let i = 0; i < occurrences; i++) {
-                if (webkitRevisionIndex < webkitRevisions.length) {
-                    const testResult: TestResult = {
-                        webkitRevision: ensure(webkitRevisions[webkitRevisionIndex],
-                            `Could not find revision #${webkitRevisionIndex}`),
-                        outcome: outcome,
-                    };
-                    lastResults.push(testResult);
-                }
-                webkitRevisionIndex++;
-            }
-        }
-
-        const newTestHistory = new TestHistory(context, testPath, lastResults, expectation);
-        collectedTestHistories.push(newTestHistory);
-    }
-
-    function traverseTestTree(root: string[], folder: JSONTestDirectory) {
-        for (let entryName in folder) {
-            if (entryName.indexOf(".") != -1 && "results" in folder[entryName] && "times" in folder[entryName]) {
-                // it's a test
-                const jsonTest = <JSONTest>folder[entryName];
-                collectTestHistory(root.concat(entryName), jsonTest);
-            } else {
-                // it's a folder
-                traverseTestTree(root.concat(entryName), <JSONTestDirectory>folder[entryName]);
-            }
-        }
-    }
-
-    traverseTestTree([], jsonReleasePlatform.tests);
-    return {
-        webkitRevisions: webkitRevisions,
-        context: context,
-        testHistories: collectedTestHistories,
-    };
-}
-
 function formatContext(context: TestContext) {
     return `${context.platform.toUpperCase()} ${BuildType[context.buildType]}`;
 }
@@ -651,10 +401,5 @@ const testContext: TestContext = {platform: "gtk", buildType: BuildType.Release}
 const allExpectations: TestExpectation[] = Array.prototype.concat.apply([], expectationFilePaths
     .map(path => parseExpectations(path)));
 
-const resultsJson: JSONTestsResultsRoot = JSON.parse(fs.readFileSync("results/gtk-release.json", "utf-8"));
-if ((<any>resultsJson).version != 4) {
-    console.warn("release.json format version has changed!");
-}
-
-const botTestsResults = constructBotTestsResultsFromJson(testContext, resultsJson);
+const botTestsResults = constructBotTestsResultsFromJson(testContext, allExpectations, "results/gtk-release.json");
 findTestsWithInvalidExpectations(botTestsResults);
---
+++ parse-results-json.ts
@@ -0,0 +1,134 @@
+import * as fs from "fs";
+import {
+    BotsTestResults,
+    BuildType,
+    Path,
+    TestContext,
+    TestExpectation,
+    TestHistory,
+    TestOutcome,
+    TestResult
+} from "./main";
+import {ensure, maxBy} from "./functional-utils";
+
+interface JSONTestsResultsRoot {
+    [platformName: string]: JSONTestsResultsPlatform;
+}
+
+interface JSONTestsResultsPlatform {
+    tests: JSONTestDirectory;
+    buildNumbers: string[];
+    webkitRevision: string[];
+}
+
+interface JSONTestDirectory {
+    [nodeName: string]: JSONTestDirectory | JSONTest;
+}
+
+interface JSONTest {
+    results: JSONTestOutcomeHistoryEntry[],
+    times: JSONTestTimesHistoryEntry[];
+}
+
+type JSONTestOutcomeLetter = string;
+type JSONTestOutcomeHistoryEntry = [
+    number, // number of occurrences
+    JSONTestOutcomeLetter // outcome
+    ];
+
+interface JSONTestTimesHistoryEntry {
+    0: number; // number of occurrences
+    1: number; // time in seconds
+}
+
+function getTestsResultsJsonPlatformName(jsonRoot: JSONTestsResultsRoot) {
+    for (let key in jsonRoot) {
+        if (key != "version") {
+            return key;
+        }
+    }
+    throw new Error("Could not find platform name in tests results JSON root.");
+}
+
+function parseOutcomeString(outcomeString: JSONTestOutcomeLetter): TestOutcome {
+    const outcomeDict: { [key: string]: TestOutcome } = {
+        "N": TestOutcome.NoData, // no data,
+        "P": TestOutcome.Pass,
+        "F": TestOutcome.Failure,
+        "C": TestOutcome.Crash,
+        "T": TestOutcome.Timeout,
+        "I": TestOutcome.ImageOnlyFailure,
+        "A": TestOutcome.Failure, // AudioOnlyFailure?
+        "O": TestOutcome.Missing,
+    };
+    if (!(outcomeString in outcomeDict)) {
+        throw new Error(`Unknown test outcome string: "${outcomeString}"`);
+    }
+    return outcomeDict[outcomeString];
+}
+
+function findMostSpecificExpectation(allExpectations: TestExpectation[],
+                                     testPath: Path,
+                                     buildType: BuildType): TestExpectation | null {
+    const matches = allExpectations.filter(expectation => expectation.matchesTest(testPath, buildType));
+    return maxBy(matches, expectation => expectation.testPath.entries.length);
+}
+
+export function constructBotTestsResultsFromJson(context: TestContext,
+                                                 allExpectations: TestExpectation[],
+                                                 testsResultsPath: string): BotsTestResults {
+    const resultsJson: JSONTestsResultsRoot = JSON.parse(fs.readFileSync(testsResultsPath, "utf-8"));
+    if ((<any>resultsJson).version != 4) {
+        console.warn("JSON format version has changed!");
+    }
+
+    const jsonReleasePlatform = resultsJson[getTestsResultsJsonPlatformName(resultsJson)];
+    const collectedTestHistories = new Array<TestHistory>();
+    const webkitRevisions = jsonReleasePlatform.webkitRevision.map(x => parseInt(x));
+    if (webkitRevisions[0] <= webkitRevisions[1]) throw new Error("assertion error");
+
+    function collectTestHistory(testPathNodes: string[], jsonTest: JSONTest) {
+        const testPath = new Path(testPathNodes);
+        const expectation = findMostSpecificExpectation(allExpectations, testPath, context.buildType);
+
+        const lastResults = new Array<TestResult>();
+        let webkitRevisionIndex = 0;
+        for (let [occurrences, outcomeString] of jsonTest.results) {
+            const outcome = parseOutcomeString(outcomeString);
+            for (let i = 0; i < occurrences; i++) {
+                if (webkitRevisionIndex < webkitRevisions.length) {
+                    const testResult: TestResult = {
+                        webkitRevision: ensure(webkitRevisions[webkitRevisionIndex],
+                            `Could not find revision #${webkitRevisionIndex}`),
+                        outcome: outcome,
+                    };
+                    lastResults.push(testResult);
+                }
+                webkitRevisionIndex++;
+            }
+        }
+
+        const newTestHistory = new TestHistory(context, testPath, lastResults, expectation);
+        collectedTestHistories.push(newTestHistory);
+    }
+
+    function traverseTestTree(root: string[], folder: JSONTestDirectory) {
+        for (let entryName in folder) {
+            if (entryName.indexOf(".") != -1 && "results" in folder[entryName] && "times" in folder[entryName]) {
+                // it's a test
+                const jsonTest = <JSONTest>folder[entryName];
+                collectTestHistory(root.concat(entryName), jsonTest);
+            } else {
+                // it's a folder
+                traverseTestTree(root.concat(entryName), <JSONTestDirectory>folder[entryName]);
+            }
+        }
+    }
+
+    traverseTestTree([], jsonReleasePlatform.tests);
+    return {
+        webkitRevisions: webkitRevisions,
+        context: context,
+        testHistories: collectedTestHistories,
+    };
+}

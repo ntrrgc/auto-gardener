016aeb278e37804888a426b846516125a0dc7f2a
Alicia Boya García <ntrrgc@gmail.com>
2017-10-12 03:16:20 +0200

    Vastly improved output

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 var require: any;
 const fs: any = require("fs");
 
-const dontMindUnexpectedPasses = true;
+const dontMindUnexpectedPasses = false;
 
 enum TestOutcome {
     Pass,
@@ -37,9 +37,93 @@ function testOutcomeToLetter(outcome: TestOutcome | null): string {
     }
 }
 
+function testOutcomeToColor(outcome: TestOutcome | null, colorType: "bg" | "fg"): string {
+    const prefix = colorType == "bg" ? "\x1b[48;5;" : "\x1b[38;5;";
+    switch (outcome) {
+        case TestOutcome.Pass:
+            return `${prefix}34m`;
+        case TestOutcome.Failure:
+            return `${prefix}9m`;
+        case TestOutcome.Crash:
+            return `${prefix}130m`;
+        case TestOutcome.Timeout:
+            return `${prefix}3m`;
+        case TestOutcome.ImageOnlyFailure:
+            return `${prefix}27m`;
+        case TestOutcome.Slow:
+            return `${prefix}0m`;
+        case null:
+            return `${prefix}255m`;
+        default:
+            throw new Error(`Unexpected outcome: ${TestOutcome[outcome]} (${outcome})`);
+    }
+}
+
 enum BuildType {
     Debug,
     Release
+}
+
+enum ToStringMode {
+    Normal = 0,
+    WithColors = 1,
+    PadBugLink = 2,
+}
+
+class TestExpectation {
+    constructor(
+        public lineNo: number,
+        public testPath: Path,
+        public bugIds: number[],
+        public expectedOutcomes: Set<TestOutcome>,
+        public buildTypeConstraint: BuildType | null = null)
+    {}
+
+    matchesTest(path: Path,buildType: BuildType): boolean {
+        return this.testPath.equalsOrContains(path) &&
+            (this.buildTypeConstraint == null || this.buildTypeConstraint == buildType);
+    }
+
+    expectedOutcomesInclude(outcome: TestOutcome): boolean {
+        return this.expectedOutcomes.has(outcome) ||
+            this.expectedOutcomes.has(TestOutcome.Skip);
+    }
+
+    toString(flags: ToStringMode, currentBgColor: string): string {
+        const bugLinkWidth = 19;
+        const parts = new Array<string>();
+
+        for (let bugId of this.bugIds) {
+            let url = `webkit.org/b/${bugId}`;
+            if (flags & ToStringMode.PadBugLink) {
+                url = vtPadLeft(url, bugLinkWidth);
+            }
+            parts.push(flags & ToStringMode.WithColors
+                ? `\x1b[38;5;219;4m${url}\x1b[38;5;255;24m`
+                : url);
+        }
+        if (flags & ToStringMode.PadBugLink && this.bugIds.length == 0) {
+            // Add empty padding with the size of a bug URL.
+            parts.push(vtPadLeft("", bugLinkWidth));
+        }
+
+        if (this.buildTypeConstraint != null) {
+            parts.push(`[ ${BuildType[this.buildTypeConstraint]} ]`);
+        }
+        parts.push(this.testPath.toString())
+
+        const outcomes = `[ ${Array.from(this.expectedOutcomes.values())
+            .map(outcome => flags & ToStringMode.WithColors
+                ? (testOutcomeToColor(outcome, "bg") + TestOutcome[outcome] + currentBgColor)
+                : TestOutcome[outcome])
+            .join(" ")
+            } ]`;
+        parts.push(flags & ToStringMode.WithColors
+            ? `\x1b[38;5;253m${outcomes}\x1b[38;5;255m`
+            : outcomes);
+
+        return parts.join(" ");
+    }
 }
 
 class Path {
@@ -63,46 +147,16 @@ class Path {
         return true;
     }
 
+    baseName() {
+        return this.entries[this.entries.length - 1];
+    }
+
+    dirName() {
+        return this.entries.slice(0, this.entries.length - 1).join("/");
+    }
+
     toString() {
         return this.entries.join("/");
-    }
-}
-
-class TestExpectation {
-    constructor(
-        public lineNo: number,
-        public testPath: Path,
-        public bugIds: number[],
-        public expectedOutcomes: Set<TestOutcome>,
-        public buildTypeConstraint: BuildType | null = null)
-    {}
-
-    matchesTest(path: Path,buildType: BuildType): boolean {
-        return this.testPath.equalsOrContains(path) &&
-            (this.buildTypeConstraint == null || this.buildTypeConstraint == buildType);
-    }
-
-    expectedOutcomesInclude(outcome: TestOutcome): boolean {
-        return this.expectedOutcomes.has(outcome) ||
-            this.expectedOutcomes.has(TestOutcome.Skip);
-    }
-
-    toString(): string {
-        const parts = new Array<string>();
-
-        for (let bugId of this.bugIds) {
-            parts.push(`webkit.org/b/${bugId}`);
-        }
-        if (this.buildTypeConstraint != null) {
-            parts.push(`[ ${BuildType[this.buildTypeConstraint]} ]`);
-        }
-        parts.push(this.testPath.toString())
-        parts.push(`[ ${Array.from(this.expectedOutcomes.values())
-            .map(x => TestOutcome[x])
-            .join(" ")
-        } ]`);
-
-        return parts.join(" ");
     }
 }
 
@@ -301,8 +355,12 @@ class TestHistory {
                 public expectation: TestExpectation | null)
     {}
 
+    getTestResult(webkitRevision: number): TestResult | null {
+        return this.lastResults.find(x => x.webkitRevision == webkitRevision) || null;
+    }
+
     matchesExpectation(webkitRevision: number): boolean | null {
-        const testResult = this.lastResults.find(x => x.webkitRevision == webkitRevision);
+        const testResult = this.getTestResult(webkitRevision);
         if (!testResult) {
             // This test has not been run in the specified revision
             return null;
@@ -327,8 +385,8 @@ class TestHistory {
 
     historyString() {
         return this.lastResults
-            .map(result => testOutcomeToLetter(result.outcome))
-            .join("");
+            .map(result => testOutcomeToColor(result.outcome, "bg") + testOutcomeToLetter(result.outcome))
+            .join("") + "\x1b[0m";
     }
 }
 
@@ -433,16 +491,139 @@ function parseReleaseJson(context: TestContext, releaseJson: ReleaseJson): BotsT
     };
 }
 
+function groupBy<K, T>(items: T[], keyFn: (item: T) => K): Map<K, T[]> {
+    const ret = new Map<K, T[]>();
+    for (let item of items) {
+        const key = keyFn(item);
+
+        if (!ret.has(key)) {
+            ret.set(key, []);
+        }
+
+        ret.get(key)!.push(item);
+    }
+    return ret;
+}
+
+function compareArrays<T>(a: T[], b: T[]): number {
+    for (let i = 0; i < a.length; i++) {
+        if (i > b.length) {
+            return 1;
+        }
+
+        if (a[i] > b[i]) {
+            return 1;
+        } else if (a[i] < b[i]) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+function poorManCompareArrayUnitTests() {
+    function comp(a: any[], b: any[], expected: number) {
+        const actual = compareArrays(a, b);
+        if (actual != expected) {
+            console.warn(`${a} - ${b} ~ ${actual} (expected ${expected})`);
+        }
+    }
+
+    comp([], [], 0);
+    comp([1], [2], -1);
+    comp([2], [1], 1);
+    comp([1], [1], 0);
+    comp(["a"], ["b"], -1);
+    comp(["b"], ["a"], 1);
+    comp(["a"], ["a"], 0);
+    comp(["a", 1], ["a", null], 1);
+    comp(["a", 1], ["a", 1], 0);
+    comp(["a", 1], ["a", 2], -1);
+}
+
+function sortedBy<T, K>(items: Iterable<T>, keyFn: (item: T) => K[]): T[] {
+    const ret = Array.from(items);
+    ret.sort((a, b) => {
+        const ka = keyFn(a);
+        const kb = keyFn(b);
+        return compareArrays(ka, kb);
+    });
+    return ret;
+}
+
+function vtPadLeft(str: string, desiredWidth: number) {
+    const widthWithoutVtCodes = str.replace(/\x1b.*?m/g, "").length;
+
+    for (let i = widthWithoutVtCodes; i < desiredWidth; i++) {
+        str += " ";
+    }
+    return str;
+}
+
+interface VtLine {
+    // Unfortunately, for a line to be covered entirely by a background color, it must be set before the end of the
+    // previous line, so we have to plan them in advance.
+    text: string;
+    bgColorCode: string;
+}
+
 function findTestsWithInvalidExpectations(botTestsResults: BotsTestResults): TestHistory[] {
     const latestRevision = botTestsResults.webkitRevisions[0];
 
     const testHistoriesWithInvalidExpectations = botTestsResults.testHistories
         .filter(history => history.matchesExpectation(latestRevision) === false);
 
-    for (let testHistory of testHistoriesWithInvalidExpectations) {
-        const expectation = testHistory.getExpectationWithDefault();
-        const outcome = testHistory.lastResults.find(r => r.webkitRevision == latestRevision)!.outcome;
-        console.log(`${expectation.toString()}, found ${TestOutcome[outcome]} ${testHistory.historyString()}`)
+    const testHistoryByOutcome = groupBy(testHistoriesWithInvalidExpectations,
+            history => history.getTestResult(latestRevision)!.outcome);
+
+    const colorReset = "\x1b[0m";
+    const lines = new Array<VtLine>();
+
+    for (let [outcome, outcomeHistories] of sortedBy(testHistoryByOutcome.entries(), ([outcome, _]) => [outcome])) {
+        const colorEven = "\x1b[48;5;8;38;5;256m";
+        const colorOdd = "\x1b[48;5;243;38;5;256m";
+
+        lines.push({text: `\x1b[1mUnexpected ${TestOutcome[outcome]}:\x1b[0m${colorEven}`, bgColorCode: colorReset});
+
+        outcomeHistories = sortedBy(outcomeHistories, (testHistory: TestHistory) => [
+            testHistory.testPath.dirName(),
+            -(testHistory.getExpectationWithDefault().bugIds[0] || -Infinity),
+            testHistory.testPath.baseName(),
+        ]);
+
+        let nextLineIsOdd = true; // Use alternating background colors to make lines easier to follow
+        let lastTestDirName: string | null = null;
+        for (let testHistory of outcomeHistories) {
+            // Add an empty line between test sets from different directories
+            if (lastTestDirName != null && testHistory.testPath.dirName() != lastTestDirName) {
+                lines.push({text: "", bgColorCode: "\x1b[48;5;237m"});
+                nextLineIsOdd = true;
+            }
+            lastTestDirName = testHistory.testPath.dirName();
+
+            const colorSufix = nextLineIsOdd ? colorOdd : colorEven;
+            lines.push({text: `${
+                vtPadLeft(testHistory.getExpectationWithDefault().toString(
+                    ToStringMode.WithColors | ToStringMode.PadBugLink,
+                    colorSufix), 130)}${
+                testHistory.historyString()}`, bgColorCode: colorSufix});
+
+            nextLineIsOdd = !nextLineIsOdd;
+        }
+
+        lines.push({text: "", bgColorCode: colorReset});
+    }
+
+    // Print lines, printing the background color code of the following one before the newline.
+    if (lines.length > 0) {
+        let i = 0;
+        console.log(lines[i].bgColorCode);
+        for (; i < lines.length; i++) {
+            const nextLineColorCode = lines[i + 1]
+                ? lines[i + 1].bgColorCode
+                : colorReset;
+
+            console.log(lines[i].text + nextLineColorCode);
+        }
     }
 
     return testHistoriesWithInvalidExpectations;
@@ -463,4 +644,4 @@ if ((<any>releaseJson).version != 4) {
 }
 
 const botTestsResults = parseReleaseJson(testContext, releaseJson);
-const testHistoriesWithInvalidExpectations = findTestsWithInvalidExpectations(botTestsResults);
+findTestsWithInvalidExpectations(botTestsResults);
